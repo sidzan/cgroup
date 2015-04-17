0a30aacf0ce0add8bfc7e7961f06138321b0882b
root <root@ip-172-31-41-245.us-west-2.compute.internal>
2015-04-17 04:59:36 +0000

    this is updated everything

--- cgroup/mount.sh
+++ cgroup/mount.sh
@@ -6,3 +6,4 @@ cgcreate -g cpuset,memory:/Batch22
 echo "0"> /sys/fs/cgroup/cpuset/Batch22/cpuset.mems
 echo "0"> /sys/fs/cgroup/cpuset/Batch22/cpuset.cpus
 
+echo "260000000" >/sys/fs/cgroup/memory/Batch22/memory.limit_in_bytes
